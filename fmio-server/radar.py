edd88845c32ab77b05d9bc590bda670ea1009e9e
Jussi Tiira <jussi@j24.fi>
2017-09-27 23:45:11 +0300

    radar figure updater first working version

--- fmio-server/radar.py
+++ fmio-server/radar.py
@@ -1,39 +1,67 @@
+#!/usr/bin/env python -p
 # coding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
+import time
 import numpy as np
-import matplotlib.pyplot as plt
 import geopandas as gpd
 import rasterio
-from matplotlib import colors
+import matplotlib.pyplot as plt
+from os import path
 from rasterio.plot import show
+from urllib.request import urlretrieve
+from j24.server import GracefulKiller
 
-plt.close('all')
-plt.ion()
+plt.ioff()
 
-with open('api.key', 'r') as keyfile:
-    key = keyfile.readline()
-radurl_base = 'http://wms.fmi.fi/fmi-apikey/{}/geoserver/Radar/ows?service=WMS&request=GetMap&format=image/geotiff&bbox=-118331.366,6335621.167,875567.732,7907751.537&width=1700&height=2500&srs=EPSG:3067&layers=Radar:suomi_dbz_eureffin'
-radurl = radurl_base.format(key)
-rad = 'Radar-suomi_dbz_eureffin.tif'
-world = gpd.read_file('ne_10m_admin_0_countries.shp')
-finland = world[world.ADMIN=='Finland']
+def generate_url(keyfilepath):
+    with open(keyfilepath, 'r') as keyfile:
+        key = keyfile.readline().splitlines()[0]
+    radurl_base = 'http://wms.fmi.fi/fmi-apikey/{}/geoserver/Radar/ows?service=WMS&request=GetMap&format=image/geotiff&bbox=-118331.366,6335621.167,875567.732,7907751.537&width=1700&height=2500&srs=EPSG:3067&layers=Radar:suomi_dbz_eureffin'
+    return radurl_base.format(key)
 
-data = rasterio.open('Radar-suomi_dbz_eureffin.tif')
-dat = data.read(1)
-mask = dat==252
-d = dat.copy()
-d[mask] = 0
-datm = np.ma.MaskedArray(data=d, mask=d==0)
-nummask = np.ma.MaskedArray(data=dat, mask=~mask)
+def read_basemap(shapefilepath):
+    world = gpd.read_file(shapefilepath)
+    return world[world.ADMIN=='Finland']
 
-cmap=colors.ListedColormap([(0.5,0.6,0.2,0.9)])
+def plot_radar_map(radar_data, basemap):
+    dat = radar_data.read(1)
+    mask = dat==252
+    d = dat.copy()
+    d[mask] = 0
+    datm = np.ma.MaskedArray(data=d, mask=d==0)
+    nummask = np.ma.MaskedArray(data=dat, mask=~mask)        
+    ax = basemap.to_crs(radar_data.read_crs().data).plot(zorder=0, color='gray')        
+    show(datm, transform=radar_data.transform, ax=ax, zorder=3)
+    show(nummask, transform=radar_data.transform, ax=ax, zorder=4, alpha=.1,
+         interpolation='bilinear')
+    ax.axis('off')
+    ax.set_xlim(left=-5e4)
+    ax.set_ylim(top=7.8e6, bottom=6.42e6)
+    return ax
 
-ax = finland.to_crs(data.read_crs().data).plot(zorder=0, color='gray')
+def update_radar_data(output_filepath):
+    data_path = '../data'
+    tmp_path = path.join(data_path, 'tmp')
+    config_path = '..'
+    keyfilepath = path.join(config_path, 'api.key')
+    radurl = generate_url(keyfilepath)
+    radarfilepath = path.join(tmp_path, 'Radar-suomi_dbz_eureffin.tif')
+    urlretrieve(radurl, filename=radarfilepath)
+    shapefile_path = path.join(data_path, 'ne_10m_admin_0_countries.shp')
+    basemap = read_basemap(shapefile_path)
+    with rasterio.open(radarfilepath) as radar_data:
+        ax = plot_radar_map(radar_data, basemap)
+    fig = ax.get_figure()
+    fig.savefig(output_filepath)
+    print('Updated {}.'.format(output_filepath))
+    plt.close(fig)
 
-show(datm, transform=data.transform, ax=ax, zorder=3)
-show(nummask, transform=data.transform, ax=ax, zorder=4, alpha=.1, interpolation='bilinear')
-ax.axis('off')
-ax.set_xlim(left=-5e4)
-ax.set_ylim(top=7.8e6, bottom=6.42e6)
+if __name__ == '__main__':
+    killer = GracefulKiller()
+    while True:
+        time.sleep(5*60)
+        update_radar_data('test.png')
+        if killer.kill_now:
+            break
