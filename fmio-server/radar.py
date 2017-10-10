e178fc3eb658db60963b3e482e42e07f0a62c7e9
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-10-10 11:52:15 +0300

    add untested setup.py, radar.py directories changed

--- fmio-server/radar.py
+++ fmio-server/radar.py
@@ -7,13 +7,12 @@ import time
 import numpy as np
 import geopandas as gpd
 import rasterio
+import tempfile
 import matplotlib.pyplot as plt
 from os import path
 from rasterio.plot import show
 from urllib.request import urlretrieve
 from j24.server import GracefulKiller
-
-plt.ioff()
 
 def generate_url(keyfilepath):
     with open(keyfilepath, 'r') as keyfile:
@@ -31,8 +30,8 @@ def plot_radar_map(radar_data, basemap):
     d = dat.copy()
     d[mask] = 0
     datm = np.ma.MaskedArray(data=d, mask=d==0)
-    nummask = np.ma.MaskedArray(data=dat, mask=~mask)        
-    ax = basemap.to_crs(radar_data.read_crs().data).plot(zorder=0, color='gray')        
+    nummask = np.ma.MaskedArray(data=dat, mask=~mask)
+    ax = basemap.to_crs(radar_data.read_crs().data).plot(zorder=0, color='gray')
     show(datm, transform=radar_data.transform, ax=ax, zorder=3)
     show(nummask, transform=radar_data.transform, ax=ax, zorder=4, alpha=.1,
          interpolation='bilinear')
@@ -41,27 +40,35 @@ def plot_radar_map(radar_data, basemap):
     ax.set_ylim(top=7.8e6, bottom=6.42e6)
     return ax
 
-def update_radar_data(output_filepath):
-    data_path = '../data'
-    tmp_path = path.join(data_path, 'tmp')
-    config_path = '..'
+def update_radar_data(output_filepath, debug=False):
+    data_path = 'data'
+    config_path = '.'
     keyfilepath = path.join(config_path, 'api.key')
     radurl = generate_url(keyfilepath)
-    radarfilepath = path.join(tmp_path, 'Radar-suomi_dbz_eureffin.tif')
-    urlretrieve(radurl, filename=radarfilepath)
-    shapefile_path = path.join(data_path, 'ne_10m_admin_0_countries.shp')
-    basemap = read_basemap(shapefile_path)
-    with rasterio.open(radarfilepath) as radar_data:
-        ax = plot_radar_map(radar_data, basemap)
-    fig = ax.get_figure()
-    fig.savefig(output_filepath)
+    with tempfile.TemporaryDirectory() as tmp_path:
+        radarfilepath = path.join(tmp_path, 'Radar-suomi_dbz_eureffin.tif')
+        urlretrieve(radurl, filename=radarfilepath)
+        shapefile_path = path.join(data_path, 'ne_10m_admin_0_countries.shp')
+        basemap = read_basemap(shapefile_path)
+        with rasterio.open(radarfilepath) as radar_data:
+            ax = plot_radar_map(radar_data, basemap)
+        fig = ax.get_figure()
+        fig.savefig(output_filepath)
     print('Updated {}.'.format(output_filepath))
-    plt.close(fig)
+    if not debug:
+        plt.close(fig)
 
 if __name__ == '__main__':
+    debug = True
+    if debug:
+        plt.ion()
+        sleep_time = 5 # seconds
+    else:
+        plt.ioff()
+        sleep_time = 5*60
     killer = GracefulKiller()
     while True:
-        time.sleep(5*60)
-        update_radar_data('test.png')
+        time.sleep(sleep_time)
+        update_radar_data('test.png', debug=debug)
         if killer.kill_now:
             break
