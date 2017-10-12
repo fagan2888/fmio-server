a68726346d7b7dcadaa7835c25ff3fc1df23e45b
Rochet2 <tqbattlenet@gmail.com>
2017-10-12 18:55:47 +0300

    Add dataminer and server files

---
+++ fmio/dataminer.py
@@ -0,0 +1,53 @@
+import threading
+from os import path, remove, listdir, makedirs
+from urllib import urlretrieve
+
+
+class DataMiner:
+    def __init__(self, key, stored_count=6):
+        self.key = key
+        self.stored_count = stored_count
+        self.tempdir = "data/tmp"
+        self.counter = 0
+        self.lock = threading.RLock()
+        with self.lock:
+            if not path.exists(self.tempdir):
+                makedirs(self.tempdir)
+            self.remove_all_files()
+
+    def remove_all_files(self):
+        with self.lock:
+            for filename in self.filenames():
+                remove(path.join(self.tempdir, filename))
+
+    @property
+    def radar_url(self):
+        url_base = 'http://wms.fmi.fi/fmi-apikey/{}/geoserver/Radar/ows?service=WMS&request=GetMap&format=image/geotiff&bbox=-118331.366,6335621.167,875567.732,7907751.537&width=1700&height=2500&srs=EPSG:3067&layers=Radar:suomi_dbz_eureffin'
+        return url_base.format(self.key)
+
+    def generate_filename(self):
+        with self.lock:
+            self.counter += 1
+            return "{}".format(self.counter)
+
+    def erase_extra_files(self):
+        with self.lock:
+            filenames = listdir(self.tempdir)
+            filenames.sort(reverse=True)
+            for filename in filenames[self.stored_count:]:
+                remove(path.join(self.tempdir, filename))
+
+    def fetch_radar_data(self):
+        with self.lock:
+            filepath = path.join(self.tempdir, self.generate_filename())
+            urlretrieve(self.radar_url, filename=filepath)
+            self.erase_extra_files()
+
+    def filenames(self):
+        with self.lock:
+            return listdir(self.tempdir)
+
+    def write_test_png(self):
+        with self.lock:
+            from shutil import copy2
+            copy2("test.png", self.tempdir)
---
+++ fmio/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, send_from_directory, send_file, url_for
+from dataminer import DataMiner
+from os import path
+import cStringIO
+import threading
+
+with open("api.key", 'r') as f:
+    key = f.readline().splitlines()[0]
+miner = DataMiner(key)
+
+
+def update_forecast():
+    miner.fetch_radar_data()
+    # build forecast after fetching the data
+    # miner.write_test_png()  # dumping some test file instead
+    timer = threading.Timer(5, update_forecast)
+    timer.daemon = True
+    timer.start()
+
+
+update_forecast()
+
+app = Flask(__name__)
+
+@app.route("/")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        url = url_for(rule.endpoint, **{x: "-{}-".format(x) for x in rule.arguments})
+        links.append("{}".format((url, rule.endpoint)))
+    return "{}".format("<br/>".join(links))
+
+@app.route("/rains/<location>/")
+def rains(location):
+    return "Not at {}".format(location)
+
+@app.route("/file")
+def file():
+    with miner.lock:
+        with open(path.join(miner.tempdir, miner.filenames()[0])) as f:
+            return send_file(cStringIO.StringIO(f.read()), mimetype="image/png")
+
+@app.route("/file2")
+def file2():
+    with miner.lock:
+        return send_from_directory(miner.tempdir, miner.filenames()[0])
