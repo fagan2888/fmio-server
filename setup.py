6dc46c184a6d9d1d3166dcd6320d9e76a54ff78d
Rochet2 <tqbattlenet@gmail.com>
2017-10-13 14:28:10 +0300

    try fix setup

--- setup.py
+++ setup.py
@@ -90,9 +90,9 @@ setup(
         'geopandas',
         'Flask',
         'gunicorn',
-        'j24==0.1.2',
+        'j24',
     ],
-    dependency_links=['https://github.com/juhi24/j24py/zipball/master#egg=j24-0.1.2'],
+    dependency_links=['https://github.com/juhi24/j24py/tarball/master#egg=j24py-0.1.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
