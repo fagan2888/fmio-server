af580ea08478759b71fe1dbfa1640f61fe4a3915
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-10-16 14:02:21 +0300

    rm deprecated script

--- fmio/save_rainmaps.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-__metaclass__ = type
-
-from j24.server import GracefulKiller
-
-
-if __name__ == '__main__':
-    pass
