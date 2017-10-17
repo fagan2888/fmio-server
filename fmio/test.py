8f6662a4b7e8a209d441a0e0949c11dd211a0f1f
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-10-17 16:53:09 +0300

    scripting

--- fmio/test.py
+++ fmio/test.py
@@ -2,13 +2,16 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
+import rasterio
 from fmio import fmi
 
 t_range = dict(starttime='2017-10-11T07:00:00Z', endtime='2017-10-11T07:30:00Z')
 
 urls = fmi.available_maps(**t_range)
-url = fmi.gen_url(timestamp='2017-10-17T07:00:00Z')
+#url = fmi.gen_url(timestamp='2017-10-17T07:00:00Z')
 
 dl = urls.tail(2)
-
 paths = fmi.download_maps(dl)
+
+rad = paths.apply(rasterio.open)
+rad.apply(fmi.plot_radar_file)
