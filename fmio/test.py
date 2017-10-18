10f9c5887fca781a6ec5747666740b05fc5f959d
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-10-18 17:51:52 +0300

    towards extrapolation

--- fmio/test.py
+++ fmio/test.py
@@ -7,9 +7,16 @@ import rasterio
 import rasterio.mask
 #import georaster
 import matplotlib.pyplot as plt
-from fmio import fmi, basemap
+from fmio import fmi, basemap, forecast
 
 plt.close('all')
+
+def plot_box_map(radar, border, box):
+    ax = fmi.plot_radar_map(radar, border=border)
+    basemap.plot_edge(box, ax=ax, zorder=10)
+    ax.set_xlim(left=1e4, right=7.8e5)
+    ax.set_ylim(top=7.8e6, bottom=6.45e6)
+
 
 def crop_raster(raster, x0, y0, x1, y1):
     win = rasterio.windows.from_bounds(left=x0, bottom=y0, right=x1, top=y1,
@@ -40,26 +47,20 @@ url = fmi.gen_url(timestamp='2017-10-17T07:00:00Z')
 dl = urls.tail(2)
 #paths = fmi.download_maps(dl)
 rads = paths.apply(rasterio.open)
-crops = []
-trans = []
+rr = []
 for i, rad in enumerate(rads.values):
     cropped, tr = crop_raster(rad, x0, y0, x1, y1)
-    crops.append(cropped)
-    trans.append(tr)
+    rr.append(forecast.rr2ubyte(cropped*0.01))
+
+
+
 f = paths.iloc[0]
 box = basemap.box(x0=x0, y0=y0, x1=x1, y1=y1)
 radar = rasterio.open(f)
-ax = fmi.plot_radar_map(radar, border=border)
-basemap.plot_edge(box, ax=ax, zorder=10)
-#pol = shapely.geometry.geo.mapping(box[0])
-#shapes = [pol]
-#cropped, transf = rasterio.mask.mask(radar, shapes=shapes, nodata=1000, crop=True)
-#meta = radar.meta.copy()
 ax = border.to_crs(radar.read_crs().data).plot(zorder=0)
 windowed, transwin = crop_raster(radar, x0=x0, y0=y0, x1=x1, y1=y1)
 rasterio.plot.show(windowed, transform=transwin, ax=ax, zorder=10)
-ax.set_xlim(left=1e4, right=7.8e5)
-ax.set_ylim(top=7.8e6, bottom=6.45e6)
+
 #rad = paths.apply(rasterio.open)
 
 
