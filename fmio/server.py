c16783c18a6601fa5b2750c32c65bc80db32d667
Rochet2 <tqbattlenet@gmail.com>
2017-10-24 15:46:23 +0300

    Avoid tkinter

--- fmio/server.py
+++ fmio/server.py
@@ -2,6 +2,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
+import matplotlib
+matplotlib.use("agg") # avoid using tkinter
+
 import cStringIO
 import json
 from os import path, environ
