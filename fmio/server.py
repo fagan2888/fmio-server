67b767c8f3d9ea66da0ae503b35c2dae0cfa7830
Rochet2 <tqbattlenet@gmail.com>
2017-10-24 14:59:28 +0300

    Add url with changing param to avoid cache

--- fmio/server.py
+++ fmio/server.py
@@ -89,9 +89,10 @@ def forecast(lon, lat):
     })
 
 
+@app.route("/rainmap/<rand>/")
 @app.route("/rainmap")
 @app.route("/rainmap.gif")
-def gif():
+def gif(**kwargs):
     if example_mode:
         return send_from_directory(DATA_DIR, "forecast.gif")
     with miner.gif_swap_lock:
